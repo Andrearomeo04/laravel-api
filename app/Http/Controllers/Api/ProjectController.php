39073fa752217d00dc641d4269c07f237daca285
andrearomeo04 <pisco004@gmail.com>
2024-10-22 21:08:08 +0200

    [UPD] created show function in api controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,4 +18,9 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+    }
 }
