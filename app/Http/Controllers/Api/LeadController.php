f3b0580db13828958ab4d6414c20918e1a1a8552
andrearomeo04 <pisco004@gmail.com>
2024-10-23 21:43:34 +0200

    [UPD] added validator

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -13,6 +13,18 @@ class LeadController extends Controller
     public function store(StoreProjectRequest $request)
     {
         $form_data = $request->all();
+
+        $validator = Validator::make($form_data, [
+            'name' => ['required', 'max:50'],
+            'surname' => ['required', 'max:100'],
+            'email' => ['required', 'max:150'],
+            'phone' => ['required', 'max:20'],
+            'content' => ['required'],
+        ],);
         
+        $new_lead = new Lead();
+        $new_lead->fill($form_data);
+
+        $new_lead->save();
     }
 }
